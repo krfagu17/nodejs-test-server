64623aacbfc3c1d4a7e26a6b075bffd5b64eda91
krfagu17 <krfagu17@gmail.com>
2024-06-22 09:14:33 +0530

    route updated

--- api/index.js
+++ api/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'home.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'about.html'));
+});
+
 app.listen(3001, () => {
     console.log('Server is running on port 30001');
 });
