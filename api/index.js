76f3bd5ed42beea9ac48c01d908c0bfdfe444f9d
krfagu17 <krfagu17@gmail.com>
2024-06-22 09:07:47 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "static" directory
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'home.html'));
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 30001');
+});
+
+module.exports = app;
